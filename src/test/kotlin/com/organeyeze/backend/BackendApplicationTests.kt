34b7969f39c332b6e21252cccc1e51396bbb27c4
Vladimir Pliska <mr.tommyjacket@gmail.com>
2023-04-24 12:35:34 +0500

    establish basic project structure

---
+++ src/test/kotlin/com/organeyeze/backend/BackendApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.organeyeze.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
